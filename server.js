b5626d281fc3606a43ace50d34526c6cb4a8491a
Paolo <paolo.alhambra@yahoo.com>
2023-03-20 21:52:15 -0700

    Layout of files. Updated schema.sql

---
+++ server.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    
+  }
+)
